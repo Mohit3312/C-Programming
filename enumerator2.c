707ee810b57e1ba8044dc81da841dbe18920c675
Mohit3312 <47062048+Mohit3312@users.noreply.github.com>
2019-02-04 13:18:05 +0530

    Add files via upload

---
+++ enumerator2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+
+enum boolean
+{
+  false,true
+};
+
+enum boolean isEven(int x)
+{
+  if(x%2==0)
+
+    return(true);
+
+  else
+
+    return(false);
+}
+
+void main()
+
+{
+  int n;
+
+  enum boolean result;
+
+  printf("Enter a number");
+
+  scanf("%d",&n);
+
+  result=isEven(n);
+
+  if(result==true)
+
+    printf("Even number");
+
+  else
+
+    printf("Odd number");
+
+  getch();
+
+}
